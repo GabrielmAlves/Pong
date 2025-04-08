6e51b51f831e95732568661f0b13bc3702bee6dd
GabrielmAlves <gabrielmalves10@gmail.com>
2025-04-07 21:06:21 -0300

    create game variables

--- PongApp/Pong.h
+++ PongApp/Pong.h
@@ -5,7 +5,24 @@
 
 class Pong
 {
-	std::shared_ptr<int> window;
-	sf::RectangleShape teste;
+	std::shared_ptr<sf::RenderWindow> window;
+	sf::RectangleShape playerOne, playerTwo;
+	sf::Texture t_playerOne, t_playerTwo, t_ball, t_field;
+	sf::Sprite field, ball;
+	sf::Vector2f velocity;
+	sf::Font font;
+	sf::Text text_playerOne, text_playerTwo;
+
+	int points_playerOne, points_playerTwo;
+	float speed;
+	bool start;
+
+protected:
+	void events();
+	void draw_game();
+	void logic();
+public:
+	Pong();
+	void run();
 };
 
